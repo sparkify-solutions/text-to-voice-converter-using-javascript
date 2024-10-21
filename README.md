5b6f41c6f20ec915677aef0872fea7ff45ca56fe
Sparkify Solutions <usamayousaf710@gmail.com>
2024-10-21 07:41:11 +0500

    adding new project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learn how to create a text to speech converter with HTML CSS and Javascript.
+<p>Create a text to speech converter</p>
+<ul>
+<li>Use HTML to Structure a text to speech converter app.</li>
+<li>Use CSS to style the text to speech converter app.</li>
+<li>For Functionality of text to speech converter app use Javascript.</li>
+</ul>
+<p>For detail explanation <a href="https://projects.sparkifysolutions.com/text-to-speech-converter/">Click Here</a></p>
---
+++ script.js
@@ -0,0 +1,19 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((voice , i) => (voiceSelect.options[i] = new Option(voice.name , i)));
+};
+
+voiceSelect.addEventListener("change" , ()=>{
+    speech.voice = voices[voiceSelect.value];
+});
+
+// Event listener for button click (to speak the text)
+document.querySelector("button").addEventListener("click", function(){
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+  });
